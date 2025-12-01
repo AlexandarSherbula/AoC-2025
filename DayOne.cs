142ec823ad5c01094637c87e24f5c9c6daea6fa1
AlexandarSherbula <acabor90@gmail.com>
2025-12-01 16:23:50 +0100

    Day one of AoC 2025 done

---
+++ DayOne.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AoC_2025
+{
+    internal class DayOne
+    {
+        public static void Run()
+        {
+            int arrowPosition = 50;
+
+            int zeroCount = 0;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                Match match = Regex.Match(line, @"\d+");
+                int prevArrowPosition = arrowPosition;
+                if (match.Success)
+                {
+                    int parsedNumber = int.Parse(match.Value);
+                    if (line.Contains('L'))
+                    {
+                        for (int i = 0; i < parsedNumber; i++)
+                        {
+                            arrowPosition--;
+                            if (arrowPosition == 0)
+                            {
+                                zeroCount++;
+                            }
+
+                            if (arrowPosition < 0)
+                            {
+                                arrowPosition += 100;
+                            }
+                        }
+                    }
+                    else if (line.Contains('R'))
+                    {
+                        for (int i = 0; i < parsedNumber; i++)
+                        {
+                            arrowPosition++;
+                            if (arrowPosition == 100)
+                            {
+                                arrowPosition -= 100;
+                                zeroCount++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(zeroCount);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using AoC_2025;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DayOne.Run();
+    }
+}
