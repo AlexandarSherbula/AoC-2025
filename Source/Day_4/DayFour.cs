19355cbba11ed391fa9a776ad2f99fbe0bfac2b3
AlexandarSherbula <acabor90@gmail.com>
2025-12-04 15:49:59 +0100

    Day 4 part 2 finished

--- Source/Day_4/DayFour.cs
+++ Source/Day_4/DayFour.cs
@@ -1,59 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 
 namespace AoC_2025
 {
     internal class DayFour
     {
+        static void ReplaceChar(string[] lines, int y, int x, char newChar)
+        {
+            if (lines == null) throw new ArgumentNullException(nameof(lines));
+            if (y < 0 || y >= lines.Length) throw new ArgumentOutOfRangeException(nameof(y));
+            if (x < 0 || x >= lines[y].Length) throw new ArgumentOutOfRangeException(nameof(x));
+
+            // Convert to char array, modify, rebuild string
+            char[] chars = lines[y].ToCharArray();
+            chars[x] = newChar;
+            lines[y] = new string(chars);
+        }
+
 
         public static void Run()
         {
+            Stopwatch sw = Stopwatch.StartNew();
             string path = Path.Combine(Directory.GetCurrentDirectory(), "Source/Day_4/input.txt");
             string[] lines = File.ReadAllLines(path);
 
             int width = lines[0].Length;
             int height = lines.Length;
 
+            List<int> removedRollPapersIndex = new List<int>();
+
             int sum = 0;
-            for (int y = 0; y < height; y++)
+            int sumPerState;
+
+            do
             {
-                for (int x = 0; x < width; x++)
+                sumPerState = 0;
+                for (int y = 0; y < height; y++)
                 {
-                    if (lines[y][x] != '@')
-                        continue;
-
-                    List<char> adjacentPositions = new List<char>();
-
-                    int[] dx = { -1, 0, 1, -1, 1, -1, 0, 1 };
-                    int[] dy = { -1, -1, -1, 0, 0, 1, 1, 1 };
-
-                    for (int i = 0; i < dx.Length; i++)
+                    for (int x = 0; x < width; x++)
                     {
-                        int newY = y + dy[i];
-                        int newX = x + dx[i];
+                        if (lines[y][x] != '@')
+                            continue;
 
-                        if (newY >= 0 && newY < lines.Length &&
-                            newX >= 0 && newX < lines[newY].Length)
+                        List<char> adjacentPositions = new List<char>();
+
+                        int[] dx = { -1, 0, 1, -1, 1, -1, 0, 1 };
+                        int[] dy = { -1, -1, -1, 0, 0, 1, 1, 1 };
+
+                        for (int i = 0; i < dx.Length; i++)
                         {
-                            adjacentPositions.Add(lines[newY][newX]);
+                            int newY = y + dy[i];
+                            int newX = x + dx[i];
+
+                            if (newY >= 0 && newY < lines.Length &&
+                                newX >= 0 && newX < lines[newY].Length)
+                            {
+                                adjacentPositions.Add(lines[newY][newX]);
+                            }
+                        }
+
+                        int rolledPaperCount = 0;
+                        for (int t = 0; t < adjacentPositions.Count; t++)
+                        {
+                            if (adjacentPositions[t] == '@')
+                            {
+                                rolledPaperCount++;
+                            }
+                        }
+
+                        if (rolledPaperCount < 4)
+                        {
+                            sumPerState++;
+                            removedRollPapersIndex.Add(y * width + x);
                         }
                     }
-
-                    int rolledPaperCount = 0;
-                    for (int t = 0; t < adjacentPositions.Count; t++)
-                    {
-                        if (adjacentPositions[t] == '@')
-                        {
-                            rolledPaperCount++;
-                        }
-                    }
-
-                    if (rolledPaperCount < 4)
-                        sum++;
                 }
-            }
+
+                sum += sumPerState;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        for (int t = 0; t < removedRollPapersIndex.Count; t++)
+                        {
+                            if (y * width + x == removedRollPapersIndex[t])
+                                ReplaceChar(lines, y, x, 'x');
+                        }
+                    }
+                }
+
+                removedRollPapersIndex.Clear();
+            } while (sumPerState != 0);
+
             Console.WriteLine(sum);
         }
     }
