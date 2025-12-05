140a7a9f2250b957d5c9df834fd7c1a57ce8358f
AlexandarSherbula <acabor90@gmail.com>
2025-12-05 19:18:40 +0100

    Modified Day 5 part 1

--- Source/Day_5/DayFive.cs
+++ Source/Day_5/DayFive.cs
@@ -1,52 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace AoC_2025
 {
     internal class DayFive
     {
+        struct Range
+        {
+            public long start;
+            public long end;
+
+            public Range(long start, long end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+        }
 
         public static void Run()
         {
             string path = Path.Combine(Directory.GetCurrentDirectory(), "Source/Day_5/input.txt");
-            var ranges = new List<string>();
-            var ingredientsID = new List<string>();
+            var ranges = new List<Range>();
+            var ingredientsID = new List<long>();
 
-            bool secondBlock = false;
+            bool firstBlock = true;
 
             foreach (var line in File.ReadLines(path))
             {
                 if (string.IsNullOrWhiteSpace(line))
                 {
-                    secondBlock = true;
+                    firstBlock = false;
                     continue;
                 }
 
-                if (!secondBlock)
-                    ranges.Add(line);
+                if (firstBlock)
+                {
+                    var parts = line.Split('-');
+                    long start = long.Parse(parts[0]);
+                    long end = long.Parse(parts[1]);
+                    ranges.Add(new Range(start, end));
+                }
                 else
-                    ingredientsID.Add(line);
+                    ingredientsID.Add(long.Parse(line));
             }
 
             int freshIngredientsCount = 0;
             foreach (var ingredient in ingredientsID)
             {
                 foreach(var range in ranges)
-                {
-                    long id = long.Parse(ingredient);
-                    var parts = range.Split('-');
-                    long start = long.Parse(parts[0]);
-                    long end = long.Parse(parts[1]);
-
-                    if (id >= start && id <= end)
+                {                    
+                    long start = range.start;
+                    long end = range.end;
+            
+                    if (ingredient >= start && ingredient <= end)
                     {
                         freshIngredientsCount++;
                         break;
                     }
                 }
             }
-
             Console.WriteLine(freshIngredientsCount);
         }
     }
